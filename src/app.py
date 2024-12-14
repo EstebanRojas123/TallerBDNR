f96406da72a18094e570ed3f10ec773faa49acc3
EstebanRojas123 <esteban.rojas01@alumnos.ucn.cl>
2024-12-14 01:47:16 -0300

    Ultima actualización

--- src/app.py
+++ src/app.py
@@ -25,8 +25,8 @@ CORS(app)
 app = Flask(__name__)
 
 NEO4J_URI = "bolt://localhost:7687"
-NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "testpassword"
+NEO4J_USER = ""
+NEO4J_PASSWORD = ""
 
 driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
 
